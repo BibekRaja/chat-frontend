fbad2778693612b5f6c8311d9f968864935081fb
BibekRaja <47000134+BibekRaja@users.noreply.github.com>
2023-12-20 22:15:13 +0545

    first commit

---
+++ src/ChatsPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="0393bfed-3917-4db5-9337-cbaf2dd1c70f"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
